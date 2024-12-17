02897dd39922c424aa505b468cf4722525db3094
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-12-17 20:56:19 +0200

    feat: add-crypto

--- apps/backend/src/modules/user/user.service.ts
+++ apps/backend/src/modules/user/user.service.ts
@@ -1,8 +1,12 @@
+import bcrypt from 'bcrypt';
+
 import { type UserSignUpRequestDto } from '../auth/libs/types/types.js';
 import { type User as TUser, type UserService } from './libs/types/types.js';
 import { type User as UserRepository } from './user.repository.js';
 
 type Constructor = Record<'userRepository', UserRepository>;
+
+const SALT_ROUNDS = 10;
 
 class User implements UserService {
   #userRepository: UserRepository;
@@ -11,8 +15,15 @@ class User implements UserService {
     this.#userRepository = userRepository;
   }
 
-  public create(payload: UserSignUpRequestDto): Promise<TUser> {
-    return this.#userRepository.create(payload);
+  public async create(payload: UserSignUpRequestDto): Promise<TUser> {
+    const hashedPassword = await bcrypt.hash(payload.password, SALT_ROUNDS);
+
+    const user = {
+      ...payload,
+      password: hashedPassword
+    };
+
+    return await this.#userRepository.create(user);
   }
 }
 
